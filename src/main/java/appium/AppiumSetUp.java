eb7676810ef8dc66a283c6b8e300566dea2b4a6b
ZenabG <zenabkale@gmail.com>
2025-03-09 09:25:32 +0100

    appium setup

--- src/main/java/appium/AppiumSetUp.java
+++ src/main/java/appium/AppiumSetUp.java
@@ -4,8 +4,11 @@ import io.appium.java_client.AppiumDriver;
 import io.appium.java_client.android.options.UiAutomator2Options;
 import io.appium.java_client.service.local.AppiumDriverLocalService;
 import java.net.MalformedURLException;
-import java.time.Duration;
 import io.github.cdimascio.dotenv.Dotenv;
+import org.openqa.selenium.remote.http.ClientConfig;
+import org.openqa.selenium.remote.service.DriverService;
+import java.net.URL;
+import java.time.Duration;
 
 public class AppiumSetUp {
 	protected AppiumDriver driver;
@@ -26,19 +29,27 @@ public class AppiumSetUp {
 				.setAppActivity(AppiumConstants.APP_ACTIVITY) // Change this
 				.setNoReset(false);
 
-		// Use AppiumDriver (no more AndroidDriver in Appium 9+)
-		driver = new AppiumDriver(service.getUrl(), options);
+		// Start Appium Service
+		service = AppiumDriverLocalService.buildService(builder);
+		service.start();
 
-		// Set implicit wait
+		System.out.println("Appium server running: " + service.isRunning());
+		System.out.println("Appium URL: " + service.getUrl().toString());
+
+		// Create ClientConfig for Selenium/Appium 9+ compatibility
+		ClientConfig clientConfig = ClientConfig.defaultConfig()
+				.baseUrl(service.getUrl())
+				.readTimeout(Duration.ofSeconds(60));
+
+		// Initialize AndroidDriver with UiAutomator2Options
+		driver = new AndroidDriver<>(service.getUrl(), options);
+
 		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(30));
+		wait = new WebDriverWait(driver, Duration.ofSeconds(20));
 	}
 
 	protected void killAppiumServer() {
-		if (driver != null) {
-			driver.quit();
-		}
-		if (service != null) {
-			service.stop();
-		}
+		service.stop();
 	}
+
 }
