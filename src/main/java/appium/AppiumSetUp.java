0a019848f562debcfa59eeb858278c201f15ae6a
ZenabG <zenabkale@gmail.com>
2025-03-09 08:28:18 +0100

    updated appium setup

--- src/main/java/appium/AppiumSetUp.java
+++ src/main/java/appium/AppiumSetUp.java
@@ -1,73 +1,48 @@
 package appium;
 
-import java.io.File;
-import java.net.MalformedURLException;
-import java.time.Duration;
-import java.util.concurrent.TimeUnit;
-
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.remote.DesiredCapabilities;
-import org.openqa.selenium.support.ui.WebDriverWait;
-
-import io.appium.java_client.android.AndroidDriver;
-import io.appium.java_client.ios.IOSDriver;
-import io.appium.java_client.remote.MobileCapabilityType;
+import io.appium.java_client.AppiumDriver;
+import io.appium.java_client.android.options.UiAutomator2Options;
 import io.appium.java_client.service.local.AppiumDriverLocalService;
-import io.appium.java_client.service.local.AppiumServiceBuilder;
-import io.appium.java_client.service.local.flags.GeneralServerFlag;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.time.Duration;
 import io.github.cdimascio.dotenv.Dotenv;
 
-public class AppiumSetUp {
-
-	protected AndroidDriver driver;  // Use AndroidDriver for Android
+public class BaseTest {
+	protected AppiumDriver driver;
 	protected WebDriverWait wait;
-	private AppiumServiceBuilder builder;
-	private AppiumDriverLocalService service;
+	protected AppiumDriverLocalService service;
+
 	private static final Dotenv dotenv = Dotenv.load();
 
 	protected void initialiseAppium() throws MalformedURLException {
+		// Start Appium server locally
+		service = AppiumDriverLocalService.buildDefaultService();
+		service.start();
 
-		DesiredCapabilities cap = new DesiredCapabilities();
+		// Set UiAutomator2 options
+		UiAutomator2Options options = new UiAutomator2Options()
+				.setPlatformName(dotenv.get("PLATFORM_NAME"))
+				.setDeviceName(dotenv.get("DEVICE_NAME"))
+				.setAppPackage(AppiumConstants.APP_PACKAGE)
+				.setAppActivity(AppiumConstants.APP_ACTIVITY) // Change this
+				.setNoReset(false);
 
-		// Set Appium desired capabilities
-		cap.setCapability(MobileCapabilityType.PLATFORM_NAME, dotenv.get("PLATFORM_NAME"));
-		cap.setCapability(MobileCapabilityType.PLATFORM_VERSION, dotenv.get("PLATFORM_VERSION"));
-		cap.setCapability(MobileCapabilityType.DEVICE_NAME, dotenv.get("DEVICE_NAME"));
-		cap.setCapability("appPackage", AppiumConstants.APP_PACKAGE);
-		cap.setCapability("appActivity", AppiumConstants.APP_ACTIVITY);
-		cap.setCapability("noReset", "false");
+		// Use AppiumDriver (no more AndroidDriver in Appium 9+)
+		driver = new AppiumDriver(service.getUrl(), options);
 
-		// Define AppiumServiceBuilder with Appium IP, port, and paths
-		builder = new AppiumServiceBuilder();
-		builder.usingDriverExecutable(new File(dotenv.get("NODE_JS_EXE_PATH")));
-		builder.withAppiumJS(new File(dotenv.get("APPIUM_JS_PATH")));
-		builder.withIPAddress("0.0.0.0");
-		builder.usingAnyFreePort();
-		builder.withCapabilities(cap);
-		builder.withArgument(GeneralServerFlag.SESSION_OVERRIDE);
-		builder.withArgument(GeneralServerFlag.LOG_LEVEL, "debug");
-		builder.withLogFile(new File(System.getProperty("user.dir") + "/Appium_Server_Logs/appium_server_logs"));
-
-		// Start the Appium server
-		try {
-			service = AppiumDriverLocalService.buildService(builder);
-			service.start();
-			service.clearOutPutStreams();
-		} catch (NullPointerException e) {
-			e.printStackTrace();
-		}
-
-		System.out.println("Appium server running : " + service.isRunning());
-		System.out.println("Appium URL " + service.getUrl().toString());
-
-		// Initialize driver using AndroidDriver
-		driver = new AndroidDriver(service.getUrl(), cap);
-		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
-
+		// Set implicit wait
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(30));
 		wait = new WebDriverWait(driver, Duration.ofSeconds(20));
 	}
 
-	public void killAppiumServer() {
-		service.stop();
+	protected void tearDown() {
+		if (driver != null) {
+			driver.quit();
+		}
+		if (service != null) {
+			service.stop();
+		}
 	}
 }
