e4016737c324e29bbe53227ba30c6a82d6257a2c
ZenabG <zenabkale@gmail.com>
2025-03-10 13:03:36 +0100

    fix no such element

--- src/main/java/com/monefy/pages/HomePage.java
+++ src/main/java/com/monefy/pages/HomePage.java
@@ -1,10 +1,11 @@
 package com.monefy.pages;
 
-import org.openqa.selenium.support.ui.WebDriver;
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class HomePage {
 
