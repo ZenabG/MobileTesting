6688e455d263f7bdcba6dade370cde04528b8c65
ZenabG <zenabkale@gmail.com>
2025-03-10 06:54:24 +0100

    updated workflow

--- src/main/java/com/monefy/appium/AppiumSetUp.java
+++ src/main/java/com/monefy/appium/AppiumSetUp.java
@@ -4,13 +4,14 @@ import io.appium.java_client.android.AndroidDriver;
 import io.appium.java_client.android.options.UiAutomator2Options;
 import io.appium.java_client.remote.MobileCapabilityType;
 import io.appium.java_client.service.local.AppiumDriverLocalService;
-
-import java.io.File;
-
 import io.appium.java_client.service.local.AppiumServiceBuilder;
 import io.appium.java_client.service.local.flags.GeneralServerFlag;
 import io.github.cdimascio.dotenv.Dotenv;
-
+import java.io.File;
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.time.Duration;
 import java.util.logging.Logger;
 
@@ -22,10 +23,8 @@ public class AppiumSetUp {
 	private static AppiumDriverLocalService service;
 	private static final Dotenv dotenv = Dotenv.load();
 
-	public static void createAndroidDriver () {
-		startServer();
+	public static void createAndroidDriver() throws MalformedURLException {
 		UiAutomator2Options options = new UiAutomator2Options();
-
 		// Set Appium desired capabilities
 		options.setCapability(MobileCapabilityType.PLATFORM_NAME, dotenv.get("PLATFORM_NAME"));
 		options.setCapability(MobileCapabilityType.PLATFORM_VERSION, dotenv.get("PLATFORM_VERSION"));
@@ -33,12 +32,39 @@ public class AppiumSetUp {
 		options.setCapability("appPackage", AppiumConstants.APP_PACKAGE);
 		options.setCapability("appActivity", AppiumConstants.APP_ACTIVITY);
 		options.setCapability("noReset", "false");
-
-		driver = new AndroidDriver(service.getUrl(), options);
+		String appiumUrl = getAppiumServerUrl();
+		driver = new AndroidDriver(new URL(appiumUrl), options);
 		setupDriverTimeouts();
 	}
 
-	protected static void startServer() {
+	private static String getAppiumServerUrl() {
+		String ciAppiumUrl = "http://localhost:4723/wd/hub";
+		String ciEnv = dotenv.get("CI");
+		// Check if CI is set to true
+		if ("true".equalsIgnoreCase(ciEnv) && isAppiumRunning(ciAppiumUrl)) {
+			System.out.println("Using Appium server in Docker (CI/CD mode).");
+			return ciAppiumUrl;  // Use Docker Appium in CI
+		} else {
+			System.out.println("Starting local Appium server...");
+			startLocalServer();  // Start Appium locally for local dev
+			return service.getUrl().toString();
+		}
+	}
+
+	private static boolean isAppiumRunning(String url) {
+		try {
+			HttpURLConnection connection = (HttpURLConnection) new URL(url).openConnection();
+			connection.setRequestMethod("HEAD");
+			connection.setConnectTimeout(3000);
+			connection.setReadTimeout(3000);
+			connection.connect();
+			return connection.getResponseCode() == 200;
+		} catch (IOException e) {
+			return false;
+		}
+	}
+
+	protected static void startLocalServer() {
 		// Start Appium server locally
 		AppiumServiceBuilder builder = new AppiumServiceBuilder();
 		builder.withIPAddress("127.0.0.1").usingAnyFreePort().withAppiumJS(new File(dotenv.get("APPIUM_JS_PATH"))).usingDriverExecutable(new File(dotenv.get("NODE_JS_EXE_PATH"))).withArgument(BASEPATH, "/wd/hub").withArgument(GeneralServerFlag.SESSION_OVERRIDE).withArgument(GeneralServerFlag.LOG_LEVEL, "debug").withLogFile(new File(System.getProperty("user.dir") + "/Appium_Server_Logs/appium_server_logs"));
