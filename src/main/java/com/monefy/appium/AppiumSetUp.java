224ba2dbd510b8b874f785548a9cd606800c930b
ZenabG <zenabkale@gmail.com>
2025-03-10 09:40:53 +0100

    fix appium url

--- src/main/java/com/monefy/appium/AppiumSetUp.java
+++ src/main/java/com/monefy/appium/AppiumSetUp.java
@@ -39,7 +39,7 @@ public class AppiumSetUp {
 	}
 
 	private static String getAppiumServerUrl() {
-		String ciAppiumUrl = "http://localhost:4723/";
+		String ciAppiumUrl = "http://localhost:4723/wd/hub";
 		String ciEnv = System.getenv().get("CI");
 		// Check if CI is set to true
 		if ("true".equalsIgnoreCase(ciEnv)) {
