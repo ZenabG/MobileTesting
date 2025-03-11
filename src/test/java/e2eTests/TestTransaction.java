ccba45a5b9829f898143d43fbc2d4fe90ab78a38
ZenabG <zenabkale@gmail.com>
2025-03-11 02:10:03 +0100

    update test

--- src/test/java/e2eTests/TestTransaction.java
+++ src/test/java/e2eTests/TestTransaction.java
@@ -32,69 +32,64 @@ public class TestTransaction extends AppiumSetUp {
         log = Logger.getLogger("global");
         wait = new WebDriverWait(driver, Duration.ofSeconds(10));
         reporterPluginSetUp = new ReporterPluginSetUp();
-    }
-
-    @BeforeMethod
-    public void initialise() {
         pageOperations = new PageOperations(driver, wait);
     }
 
-    @Test
-    public void testAddIncome() throws MalformedURLException, InterruptedException {
+    @BeforeMethod
+    public void initialise() throws InterruptedException {
         pageOperations.skipOffers();
-        pageOperations.addSalaryIncome("2500");
-//        pageOperations.addCarMortgageExpense("500");
-        Thread.sleep(4000);
-        String actualIncomeBalance = pageOperations.getBalanceText();
-        System.out.println("Actual Income Balance: " + actualIncomeBalance);
-
-        // Remove currency symbol, commas, and decimal point
-        actualIncomeBalance = actualIncomeBalance.replaceAll("[^\\d]", "");
-
-        // Convert to integer and divide by 100 to remove trailing zeroes
-        int balance = Integer.parseInt(actualIncomeBalance) / 100;
-
-        assertEquals("2500", String.valueOf(balance));
     }
 
-//    @Test
-//    public void testUpdateTransaction() throws MalformedURLException, InterruptedException {
-//
-//        pageOperations.BalanceButton();
-//        pageOperations.addSalaryIncome("2500");
-//        pageOperations.updateSalaryIncome("3000");
-//        String actualIncomeBalance = pageOperations.getIncomeBalance();
-//
-//        // Remove currency symbol, commas, and decimal point
-//        actualIncomeBalance = actualIncomeBalance.replaceAll("[^\\d]", "");
-//
-//        assertEquals("300000", actualIncomeBalance);
-//    }
+    @Test
+    public void testAddIncome() throws InterruptedException {
+        pageOperations.addSalaryIncome("2500");
+//        Thread.sleep(4000);
+        String actualIncomeBalance = pageOperations.getBalanceText();
+//        System.out.println("Actual Income Balance: " + actualIncomeBalance);
+
+        actualIncomeBalance = getTrimmedAmount(actualIncomeBalance);
+
+        assertEquals("2500", actualIncomeBalance);
+    }
+
+    @Test
+    public void testUpdateBalanceByAddingExpense() throws MalformedURLException, InterruptedException {
+        pageOperations.addCarMortgageExpense("500");
+//        Thread.sleep(4000);
+        String actualIncomeBalance = pageOperations.getBalanceText();
+//        System.out.println("Actual Income Balance: " + actualIncomeBalance);
+
+        actualIncomeBalance = getTrimmedAmount(actualIncomeBalance);
+
+        assertEquals("2000", actualIncomeBalance);
+    }
+
+    @AfterMethod
+    public void tearDown(ITestResult result) throws IOException {
+        if (result.getStatus() == ITestResult.FAILURE) {
+            reporterPluginSetUp.setTestInfo(getAppiumServerUrl(), driver.getSessionId().toString(), result.getMethod().getMethodName(), "FAIL", result.getThrowable().getMessage());
+        } else {
+            reporterPluginSetUp.setTestInfo(getAppiumServerUrl(), driver.getSessionId().toString(), result.getMethod().getMethodName(), "PASS", null);
+        }
+    }
 
 
     @AfterSuite
     public void stopAppiumServer(ITestContext context) throws IOException {
-        String appiumUrl = getAppiumServerUrl();
-        String ciEnv = System.getenv().get("CI");
-        // Check if CI is set to true
-        if ("true".equalsIgnoreCase(ciEnv)) {
+        String report = reporterPluginSetUp.getReport(getAppiumServerUrl());
+        reporterPluginSetUp.createReportFile(report, "report");
 
-            if (driver != null) {
-                for (ITestResult result : context.getPassedTests().getAllResults()) {
-                    reporterPluginSetUp.setTestInfo(appiumUrl, driver.getSessionId().toString(), result.getMethod().getMethodName(), "PASS", null);
-                }
-                for (ITestResult result : context.getFailedTests().getAllResults()) {
-                    reporterPluginSetUp.setTestInfo(appiumUrl, driver.getSessionId().toString(), result.getMethod().getMethodName(), "FAIL", result.getThrowable().getMessage());
-                }
-                for (ITestResult result : context.getSkippedTests().getAllResults()) {
-                    reporterPluginSetUp.setTestInfo(appiumUrl, driver.getSessionId().toString(), result.getMethod().getMethodName(), "SKIP", null);
-                }
-                driver.quit();
-            }
-            String report = reporterPluginSetUp.getReport(appiumUrl);
-            reporterPluginSetUp.createReportFile(report, "report");
-        }
         driver.quit();
         killAppiumServer();
+    }
+
+    private String getTrimmedAmount(String amount) {
+        // Remove currency symbol, commas, and decimal point
+        amount = amount.replaceAll("[^\\d]", "");
+
+        // Convert to integer and divide by 100 to remove trailing zeroes
+        int balance = Integer.parseInt(amount) / 100;
+
+        return String.valueOf(balance);
     }
 }
