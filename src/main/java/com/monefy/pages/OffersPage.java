a62017ea1098f347223a08d11b9a50451bae8303
ZenabG <zenabkale@gmail.com>
2025-03-11 07:10:23 +0100

    update test

--- src/main/java/com/monefy/pages/OffersPage.java
+++ src/main/java/com/monefy/pages/OffersPage.java
@@ -4,6 +4,7 @@ import io.appium.java_client.android.AndroidDriver;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.WebDriverWait;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
